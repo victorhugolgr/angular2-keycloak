69db6306023d8d09280ecf1ef238ebbd6fb75994
victorhugolgr <victorhugolgr@gmail.com>
2017-02-23 10:04:32 -0300

    autencição com o keycloak funcionando.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,47 @@
+import './polyfills.ts';
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import * as Keycloak from 'keycloak-js';
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+let keycloak = Keycloak('keycloak/keycloak.json');
+window['_keycloak'] = keycloak;
+
+window['_keycloak'].init(
+    {onLoad: 'login-required'}
+)
+    .success(function (authenticated) {
+
+      if (!authenticated) {
+        window.location.reload();
+      }
+
+      // refresh login
+      setInterval(function () {
+
+        keycloak.updateToken(70).success(function (refreshed) {
+          if (refreshed) {
+            console.log('Token refreshed');
+          } else {
+            console.log('Token not refreshed, valid for '
+                + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
+          }
+        }).error(function () {
+          console.error('Failed to refresh token');
+        });
+
+      }, 60000);
+
+      console.log("Loading...");
+
+      platformBrowserDynamic().bootstrapModule(AppModule);
+
+    })
